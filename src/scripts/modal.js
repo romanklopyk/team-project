565c2cd76700b52c7a0a6562fe4f70b75751e637
romankhlopyk <romankhlopyk@outlook.com>
2026-05-03 13:02:28 +0300

    add js, id, fix-modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,21 @@
-console.log('modal.js is connected');
+function handleModal() {
+  const $modal ={
+    close: document.querySelector('.modal-close-button'),
+    open: document.querySelector('.burger-btn'),
+    menu: document.querySelector('.modal'),
+    items: document.querySelectorAll('.modal-item-link'),
+  }
+  $modal.close.addEventListener('click', () => {
+    $modal.menu.classList.remove('is-open');
+  });
+  $modal.open.addEventListener('click', () => {
+    $modal.menu.classList.add('is-open');
+  });
+  $modal.items.forEach(item => {
+    item.addEventListener('click', () => {
+      $modal.menu.classList.remove('is-open');
+    });
+  });
+}
+
+handleModal();
